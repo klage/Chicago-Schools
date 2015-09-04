03615305102ee9deaaf1a109e03d6f8479b28be9
kyle <klage@luc.edu>
2015-09-04 11:09:29 -0500

    added more

--- chicago.py
+++ chicago.py
@@ -1,6 +1,10 @@
 import csv
 with open("ho.csv",'r')as f:
-    dict = csv.DictReader(f, delimiter=',', quotechar='"')
-    print (dict)
-    for line in dict:
-        print(line['NAME'])
+    with open("Sc.csv",'r')as g:
+        dict = csv.DictReader(f, delimiter=',', quotechar='"')
+        dict2 = csv.DictReader (g, delimiter=',', quotechar='"')
+        print (dict)
+        for line in dict2:
+            print(line['SCHOOL NAME'])
+        for line in dict:
+            print(line['NAME'])
